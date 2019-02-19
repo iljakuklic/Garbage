cd026e58a3d821744ca5b267d191a7694f9304c4
LukasK <ilja.kuklic@centrum.cz>
2019-02-19 14:00:42 +0000

    freer-final-encoding

--- Freer.hs
+++ Freer.hs
@@ -1,6 +1,8 @@
 
 {-# LANGUAGE GADTs, NoMonomorphismRestriction, RankNTypes #-}
 
+import qualified Control.Monad.State as S
+
 data Mon f a where
   Pure :: a -> Mon f a
   Bind :: f a -> (a -> Mon f b) -> Mon f b
@@ -21,7 +23,7 @@ instance Functor (Mon f) where
 
 run :: (forall a . a -> g a)
     -> (forall a b . f a -> (a -> g b) -> g b)
-	-> Mon f a -> g a
+    -> Mon f a -> g a
 run pur alg = run'
   where
     run' (Pure x) = pur x
@@ -51,10 +53,27 @@ runSte (Pure x) s = (s, x)
 runSte (Bind Get k) s = runSte (k s) s
 runSte (Bind (Set s') k) s = runSte (k ()) s'
 
-newtype G s a = G (s -> (s, a))
-runSte' :: Mon (Ste s) a -> G s a
-runSte' = run (\x -> G (\s -> (s, x))) _alg
+-- Freer monad final encoding
+newtype Freer f a = Freer {
+  runFreer :: forall m. Monad m => (forall b. f b -> m b) -> m a }
 
-steAlg :: Ste s a -> (a -> G s b) -> G s b
-steAlg Get     k = k _get
-steAlg (Set x) k = _set
+instance Functor (Freer f) where
+  fmap f gm = Freer (\x -> fmap f (runFreer gm x))
+
+instance Applicative (Freer f) where
+  pure x = Freer (const (pure x))
+  ff <*> xx = Freer (\k -> (runFreer ff k) <*> (runFreer xx k))
+
+instance Monad (Freer f) where
+  return = pure
+  ma >>= f = Freer (\k -> runFreer ma k >>= \x -> runFreer (f x) k)
+
+interpState :: Ste s a -> S.State s a
+interpState Get = S.get
+interpState (Set x) = S.put x
+
+runF :: Monad m => (forall b. f b -> m b) -> Freer f a -> m a
+runF nt act = runFreer act nt
+
+runSteF :: Freer (Ste s) a -> S.State s a
+runSteF = runF interpState
