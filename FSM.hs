60d57a4470f900cf18b840d4bd685b1832ef7a38
LukasK <ilja.kuklic@centrum.cz>
2021-07-30 09:51:13 +0100

    Add an example to FSM.hs

--- FSM.hs
+++ FSM.hs
@@ -6,6 +6,7 @@ import Data.Monoid
 import Data.Foldable
 import Data.List (nub)
 
+-- State transition function
 newtype TF s = TF { getTF :: Array s s }
   deriving (Eq, Ord, Show)
 type Ste s = (Bounded s, Enum s, Ix s)
@@ -13,9 +14,11 @@ type Ste s = (Bounded s, Enum s, Ix s)
 mkTF :: Ste s => [s] -> TF s
 mkTF tf = TF $ array (minBound, maxBound) (zip [minBound..maxBound] tf)
 
+instance Ste s => Semigroup (TF s) where
+  (TF a) <> (TF b) = TF (fmap (b!) a)
+
 instance Ste s => Monoid (TF s) where
   mempty = mkTF [minBound..maxBound]
-  mappend (TF a) (TF b) = TF (fmap (b!) a)
 
 eqfix f = \x -> let fx = f x in if fx == x then x else eqfix f fx
 
@@ -45,6 +48,18 @@ strNL   = mkTF [ Out, Out, Out, Bad, Bad, Out, Bad ]
 strDot  = mkTF [ Bad, Frc, Bad, Str, Bad, Cmt, Bad ]
 
 strTFs = closure (fromList [strChar, strQuot, strBs, strSpc, strTN, strNum, strUS, strHash, strNL, strDot])
+
+data UTF8Ste = U0 | U1 | U2 | U3 | UX
+     deriving (Eq, Ord, Bounded, Enum, Ix, Show)
+
+--                 U0
+u0xxxxxxx = mkTF [ U0, U0, U0, U0, U0 ]
+u10xxxxxx = mkTF [ UX, U0, U1, U2, UX ]
+u110xxxxx = mkTF [ U1, U1, U1, U1, U1 ]
+u1110xxxx = mkTF [ U2, U2, U2, U2, U2 ]
+u11110xxx = mkTF [ U3, U3, U3, U3, U3 ]
+
+utf8TFs = closure (fromList [u0xxxxxxx, u10xxxxxx, u110xxxxx, u1110xxxx, u11110xxx])
 
 printTFs :: Show s => Foldable t => t (TF s) -> IO ()
 printTFs = mapM_ (print . toList . getTF)
